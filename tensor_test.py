7db4cf1e456a05cd7a3bf72ff4c0b00f871383be
윤경섭 <ksyoon70@gmail.com>
2021-06-16 19:25:12 +0900

    수정[M018]
    - ppt 수정

---
+++ tensor_test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 19:16:56 2021
+
+@author: user
+"""
+import numpy as np
+
+rois = np.random.randint(4,size=(1,2,4))
+print(rois)
+
+image_meta =  np.random.randint(5,size=(2,14))
+print(image_meta)
+
+result1 = [rois,image_meta]
+print(result1)
