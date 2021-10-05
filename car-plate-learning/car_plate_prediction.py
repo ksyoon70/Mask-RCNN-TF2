76ccc8e14d5dca535491e0782d8d7e745b67526e
윤경섭 <ksyoon70@gmail.com>
2021-10-05 14:20:30 +0900

    수정 [M0314]
    - 차량번호판검지 보고서 추가
    - test 영상 저장하도록 변경

--- car-plate-learning/car_plate_prediction.py
+++ car-plate-learning/car_plate_prediction.py
@@ -1,19 +1,141 @@
+import random
 import mrcnn
 import mrcnn.config
 import mrcnn.model
 import mrcnn.visualize
+import colorsys
 import cv2
 import os
 import matplotlib.pyplot as plt
 import skimage.color
 import skimage.io
 import skimage.transform
+import numpy as np
+from skimage.measure import find_contours
+import matplotlib.pyplot as plt
+from matplotlib import patches,  lines
+from matplotlib.patches import Polygon
 
 # load the class label names from disk, one label per line
 # CLASS_NAMES = open("coco_labels.txt").read().strip().split("\n")
 
 #CLASS_NAMES = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 CLASS_NAMES = ['BG','car','plate','kangaroo']
+
+show_images = False
+
+
+def random_colors(N, bright=True):
+    """
+    Generate random colors.
+    To get visually distinct colors, generate them in HSV space then
+    convert to RGB.
+    """
+    brightness = 1.0 if bright else 0.7
+    hsv = [(i / N, 1, brightness) for i in range(N)]
+    colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
+    random.shuffle(colors)
+    return colors
+
+def apply_mask(image, mask, color, alpha=0.5):
+    """Apply the given mask to the image.
+    """
+    for c in range(3):
+        image[:, :, c] = np.where(mask == 1,
+                                  image[:, :, c] *
+                                  (1 - alpha) + alpha * color[c] * 255,
+                                  image[:, :, c])
+    return image
+
+def display_instances(image, boxes, masks, class_ids, class_names,
+                      scores=None, title="",
+                      figsize=(16, 16), ax=None,
+                      show_mask=True, show_bbox=True,
+                      colors=None, captions=None,save_fig_path=None):
+    """
+    boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
+    masks: [height, width, num_instances]
+    class_ids: [num_instances]
+    class_names: list of class names of the dataset
+    scores: (optional) confidence scores for each box
+    title: (optional) Figure title
+    show_mask, show_bbox: To show masks and bounding boxes or not
+    figsize: (optional) the size of the image
+    colors: (optional) An array or colors to use with each object
+    captions: (optional) A list of strings to use as captions for each object
+    """
+    # Number of instances
+    N = boxes.shape[0]
+    if not N:
+        print("\n*** No instances to display *** \n")
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
+
+    # If no axis is passed, create one and automatically call show()
+    auto_show = False
+    if not ax:
+        _, ax = plt.subplots(1, figsize=figsize)
+        auto_show = True
+
+    # Generate random colors
+    colors = colors or random_colors(N)
+
+    # Show area outside image boundaries.
+    height, width = image.shape[:2]
+    ax.set_ylim(height + 10, -10)
+    ax.set_xlim(-10, width + 10)
+    ax.axis('off')
+    ax.set_title(title)
+
+    masked_image = image.astype(np.uint32).copy()
+    for i in range(N):
+        color = colors[i]
+
+        # Bounding box
+        if not np.any(boxes[i]):
+            # Skip this instance. Has no bbox. Likely lost in image cropping.
+            continue
+        y1, x1, y2, x2 = boxes[i]
+        if show_bbox:
+            p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
+                                alpha=0.7, linestyle="dashed",
+                                edgecolor=color, facecolor='none')
+            ax.add_patch(p)
+
+        # Label
+        if not captions:
+            class_id = class_ids[i]
+            score = scores[i] if scores is not None else None
+            label = class_names[class_id]
+            caption = "{} {:.3f}".format(label, score) if score else label
+        else:
+            caption = captions[i]
+        ax.text(x1, y1 + 8, caption,
+                color='w', size=11, backgroundcolor="none")
+
+        # Mask
+        mask = masks[:, :, i]
+        if show_mask:
+            masked_image = apply_mask(masked_image, mask, color)
+
+        # Mask Polygon
+        # Pad to ensure proper polygons for masks that touch image edges.
+        padded_mask = np.zeros(
+            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+        padded_mask[1:-1, 1:-1] = mask
+        contours = find_contours(padded_mask, 0.5)
+        for verts in contours:
+            # Subtract the padding and flip (y, x) to (x, y)
+            verts = np.fliplr(verts) - 1
+            p = Polygon(verts, facecolor="none", edgecolor=color)
+            ax.add_patch(p)
+    ax.imshow(masked_image.astype(np.uint8))
+    if not (save_fig_path is None):
+        plt.savefig(save_fig_path, bbox_inches="tight")
+    if auto_show:
+        plt.show()
+
+
 class SimpleConfig(mrcnn.config.Config):
     # Give the configuration a recognizable name
     NAME = "coco_inference"
@@ -49,6 +171,12 @@ images_dir = os.path.join(test_dataset_dir,'images') #dataset_dir + '/images/'
 if not os.path.isdir(images_dir):
 	os.mkdir(images_dir)
 
+   
+result_dir = os.path.join(test_dataset_dir,'result') #dataset_dir + '/images/'
+#디렉토리가 없으면 만든다.
+if not os.path.isdir(result_dir):
+	os.mkdir(result_dir)    
+
 for filename in os.listdir(images_dir):
 	# load the input image, convert it from BGR to RGB channel
 	image_path = os.path.join(images_dir,filename)
@@ -67,14 +195,17 @@ for filename in os.listdir(images_dir):
 		
 		# Get the results for the first image.
 		r = r[0]
+
+		result_path = os.path.join(result_dir,filename)
 		
 		# Visualize the detected objects.
-		mrcnn.visualize.display_instances(image=image, 
-										boxes=r['rois'], 
-										masks=r['masks'], 
-										class_ids=r['class_ids'], 
-										class_names=CLASS_NAMES, 
-										scores=r['scores'])
+		display_instances(image=image, 
+						boxes=r['rois'], 
+						masks=r['masks'], 
+						class_ids=r['class_ids'], 
+						class_names=CLASS_NAMES, 
+						scores=r['scores'],save_fig_path=result_path)
+
 	else :
 		print('image path : {} is not exist!'.format(image_path))
 	
